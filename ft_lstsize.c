e1d1833fc4f665ff72119ff0fa7b3269b73a1f78
srngch <srngch.dev@gmail.com>
2021-08-05 03:58:33 +0900

    Modify ft_lstsize.c Fixed a return value 1 to 0 in the situations: lst is not allocated memory as t_list, only a t_list pointer

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -6,7 +6,7 @@
 /*   By: sarchoi <sarchoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/07 17:36:57 by sarchoi           #+#    #+#             */
-/*   Updated: 2021/05/07 19:02:58 by sarchoi          ###   ########.fr       */
+/*   Updated: 2021/08/05 03:58:14 by sarchoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_lstsize(t_list *lst)
 	int	i;
 
 	i = 0;
-	if (!lst)
+	if (!lst->content || !lst)
 		return (0);
 	while (lst->next)
 	{
